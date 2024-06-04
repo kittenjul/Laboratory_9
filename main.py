d76283b99b4423af07f42e4dcf07f3b97de41080
Iuliia Kunitsyna <kunitsynauv@gmail.com>
2024-06-04 10:55:15 +0300

    edit main

--- main.py
+++ main.py
@@ -2,72 +2,37 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import csv
 
+def get_empty_data(lines, index):
+    miss = 0
+    total = len(lines)
+    share = 0
+    for line in lines:
+        lst = line.strip().split(',')
+        if lst[index] == '':
+            miss += 1
+            share = miss / total * 100
+    return miss, share
+
 st.image('30-1-1204x483.jpg')
+columns = ['PassengerId',	'Survived',	'Pclass',	'Name', 'Sex',	'Age',	'SibSp', 'Parch',	'Ticket',	'Fare',	'Cabin',	'Embarked']
 st.title('Отсутствие данных о пассажирах')
 st.write('Для просмотра информации о количестве пассажиров, по которым нет данных, выберите соответствующую категорию')
-choice = st.selectbox('Выберите категорию:', ['PassengerId',	'Survived',	'Pclass',	'Name',	'Sex',	'Age',	'SibSp','Parch',	'Ticket',	'Fare',	'Cabin',	'Embarked'])
-
-columns = ['PassengerId',	'Survived',	'Pclass',	'Name',	'Sex',	'Age',	'SibSp',
-           'Parch',	'Ticket',	'Fare',	'Cabin',	'Embarked']
-
+choice = st.selectbox('Выберите категорию:', columns)
 index = columns.index(choice)
 
-def get_empty_data(lines):
-    missing = 0
-    total = 0
-    val = 0
-    lines = csv.reader(file)
-    for line in lines:
-        total += 1
-        if line[index] == '':
-            missing += 1
-            val = missing / total * 100
-    return missing, val
+if index > 3:
+    index += 1
 
 with open('data.csv') as file:
-    lines = index
-    missing, val = get_empty_data(lines)
+    lines = file.readlines()[1:]
 
+miss, share = get_empty_data(lines, index)
+data = {'Категория':['Количество пассажиров без данных', 'Доля,%'], 'Данные': [miss, share]}
+st.table(data)
 
-if choice == 'PassengerId':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
-if choice == 'Survived':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
-if choice == 'Pclass':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
-if choice == 'Name':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
-if choice == 'Sex':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
-if choice == 'Age':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%':val}
-    st.table(data)
-if choice == 'SibSp':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
-if choice == 'Parch':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
-if choice == 'Ticket':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
-if choice == 'Fare':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
-if choice == 'Cabin':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
-if choice == 'Embarked':
-    data = {'Категория': choice, 'Количество пассажиров без данных': missing, 'Доля,%': val}
-    st.table(data)
 
 fig = plt.figure(figsize=(10, 5))
-plt.bar(choice, missing)
+plt.bar(choice, miss)
 plt.xlabel("Наименование категории")
 plt.ylabel("Количество пассажиров")
 plt.title("Количество пассажиров, по которым отсутствуют данные")
--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 from main import get_empty_data
 
 def test_get_empty_data():
-    lines = ['PassengerId,Survived,Pclass',
+    lines = ['1,2,3',
              '1,'',3',
              '1,'',''']
-    assert get_empty_data(lines[2]) == (0,0)
+    assert get_empty_data(lines) == (0,0)

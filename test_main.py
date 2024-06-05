87b0bac6adf7d3bb790ef8833425b43a1afe8a7e
Iuliia Kunitsyna <kunitsynauv@gmail.com>
2024-06-05 22:05:52 +0300

    Add test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,24 @@
 from main import get_empty_data
 
 def test_get_empty_data():
-    lines = ['1,2,3',
-             '1,'',3',
-             '1,'',''']
-    assert get_empty_data(lines) == (0,0)
+    lines = ['1,2,3,4,5,6,7,8',
+             '1,2,3,5,0,'',0,0',
+             '1,2,0,5,8,'','',0',
+             '0,0,0,0,0,0,0,0',]
+    assert get_empty_data(lines, 5) == (2,50)
+
+
+def test_get_empty_data_2():
+    lines = ['1,2,3,4,5,6,7,8',
+             '1,2,3,5,0,'',0,0',
+             '1,2,0,5,8,'','',0',
+             '0,0,0,'',0,0,0,0',]
+    assert get_empty_data(lines, 4) == (0,0)
+
+
+def test_get_empty_data_3_false():
+    lines = ['1,2,3,4,5,6,7,8',
+             '1,2,3.2,5,0,'',0,0',
+             '1,2,0,5,8,'','',0',
+             '0,0,0,0,0,0,0,0',]
+    assert get_empty_data(lines, 2) == (0,0)
